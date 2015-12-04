c4a46ffb1e8f6cfcb9e9e1ab8a5fdc1a8e8f4756
David Mohl <me@dave.cx>
2015-12-04 18:59:07 +0900

    version bump

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject ajom/lein-template "0.1.0"
+(defproject ajom/lein-template "0.1.1"
   :description "Template for atom plugins in clojurescript"
   :url "https://github.com/dvcrn/ajom"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
   :eval-in-leiningen true
   :scm {:name "git"
-        :url "https://github.com/dvcrn/ajom"}
-  )
+        :url "https://github.com/dvcrn/ajom"})
